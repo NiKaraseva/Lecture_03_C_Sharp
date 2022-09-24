8139f21d3253227db4835119afbf3b4021615ded
Nika <nikarasevas@gmail.com>
2022-09-24 19:08:45 +0300

    Как работает цикл for

---
+++ for/Program.cs
@@ -0,0 +1,13 @@
+﻿// Цикл for
+
+string Method4(int count, string text)
+{
+    string result = String.Empty; // или так: "";
+    for (int i = 0; i < count; i++)
+    {
+        result = result + text;
+    }
+    return result;
+}
+string res = Method4(10, "ТекстХот");
+Console.WriteLine(res);
